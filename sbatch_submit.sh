babb09f234809e7af8b20222b71ccf1c1f241247
ashrafkasem <ashrafkasem.asu@gmail.com>
2023-12-13 20:52:23 +0100

    sbatch example submission

---
+++ sbatch_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition=maxgpu  
+
+#SBATCH --time=48:00:00
+
+#SBATCH --nodes=1
+
+#SBATCH --constraint='A100-SXM4-80GB'
+unset LD_PRELOAD
+trkml
+source venv_maxwell/bin/activate 
+python train.py configs/config_maxwell.yaml 0
