3723d84acdb4465fe869d3c827fe574eef8a8d75
ashrafkasem <ashrafkasem.asu@gmail.com>
2023-12-19 16:15:44 +0100

    make the plotter working

--- core/plotting/plotter.py
+++ core/plotting/plotter.py
@@ -4,66 +4,94 @@ import matplotlib.pyplot as plt
 import yaml
 import os
 
+
 class plotterABC:
-    def __init__(self, runs, logs_dir, output_dir, mertics=["loss"], dataTypes=["training"],formates=["pdf"]):
+    def __init__(
+        self,
+        runs,
+        logs_dir,
+        output_dir,
+        metrics=["loss"],
+        dataTypes=["training"],
+        formates=["pdf"],
+        logY={},
+        Axis_limits={},
+    ):
         self.logs_dir = logs_dir
         self.output_dir = output_dir
         self.runs = runs
         self.formates = formates
         self.dataTypes = dataTypes
-        self.mertics = mertics
+        self.metrics = metrics
         self.log_dfs = {}
         self.labels = {}
+        self.logY = logY
+        self.Axis_limits = Axis_limits
 
         for dataType in self.dataTypes:
             self.log_dfs[dataType] = {}
             self.labels[dataType] = {}
-            for run in self.runs: 
-                 self.labels[dataType][run] = self._extractLabel()
-                 self.log_dfs[dataType][run] = pd.read_csv(f"{self.logs_dir}/run{run}/log_{dataType}.csv")
+            for run in self.runs:
+                self.labels[dataType][run] = self._extractLabel(run)
+                self.log_dfs[dataType][run] = pd.read_csv(
+                    f"{self.logs_dir}/run{run}/log_{dataType}.csv"
+                )
 
         for formate in self.formates:
             if not os.path.exists(f"{self.output_dir}/{formate}"):
                 os.makedirs(f"{self.output_dir}/{formate}")
 
-    def _extractLabel(self):
+    def _extractLabel(self, run):
         label = "undefined"
-        with open(self.config, 'r') as ymlfile:
+        with open(f"{self.logs_dir}/run{run}/config.yaml", "r") as ymlfile:
             config = yaml.load(ymlfile, Loader=yaml.FullLoader)
             label = f"GNN - D={config['hid_dim']}, it={config['n_iters']}, lr={config['lr_c']}, bsize={config['batch_size']}"
-            self.dataset = config['dataset']
+            self.dataset = config["dataset"]
         return label
-        
+
     def makePlots(self):
         metric_dict = {}
-        plt.clf()
-        plt.figure()
         for metric in self.metrics:
+            plt.clf()
+            plt.figure()
             metric_dict[metric] = {}
-            for dataType in self.dataTypes: 
+            for dataType in self.dataTypes:
                 metric_dict[metric][dataType] = {}
-                for run in self.runs: 
-                    metric_dict[metric][dataType][run] = self.log_dfs[dataType][run][metric] if metric != 'auc' else  1- self.log_dfs[dataType][run][metric]
-                    plt.plot(len(metric_dict[metric][dataType][run]), metric_dict[metric][dataType][run], label = self.labels[dataType][run])
-        
+                for run in self.runs:
+                    metric_dict[metric][dataType][run] = (
+                        self.log_dfs[dataType][run][metric]
+                        if metric != "auc"
+                        else 1 - self.log_dfs[dataType][run][metric]
+                    )
+                    plt.plot(
+                        metric_dict[metric][dataType][run].index,
+                        metric_dict[metric][dataType][run],
+                        label=self.labels[dataType][run] + " " + dataType,
+                    )
+
             plt.title(f"data:{self.dataset} - GNN")
-            plt.xlabel('epochs')
-            if metric=='auc':
-                ylabel = '1-AUC (lower the better)'
-                plt.yscale('log')
-                # plt.ylim([1e-6, 1.0])
-            elif metric=='loss':
-                ylabel = 'Loss (lower the better)'
-                plt.yscale('log')
+            plt.xlabel("epochs")
+            if metric == "auc":
+                ylabel = "1-AUC (lower the better)"
+            elif metric == "loss":
+                ylabel = "Loss (lower the better)"
             else:
-                ylabel=metric
+                ylabel = metric
+
             plt.ylabel(ylabel)
-            #plt.ylim([0.2, 1.0])
+
+            if metric in self.logY and self.logY[metric] == True:
+                plt.yscale("log")
+            if metric in self.Axis_limits:
+                plt.ylim(self.Axis_limits[metric])
+
             plt.legend()
-            
+
             for formate in self.formates:
                 if formate == "png":
-                    plt.savefig(f"{self.output_dir}/{formate}/{metric}.{formate}", dpi=600)
+                    plt.savefig(
+                        f"{self.output_dir}/{formate}/{metric}.{formate}", dpi=600
+                    )
                 else:
                     plt.savefig(f"{self.output_dir}/{formate}/{metric}.{formate}")
             plt.close()
--- run_plotter.py
+++ run_plotter.py
@@ -1,13 +1,29 @@
 from core.plotting.plotter import plotterABC
 
-if __name__=='__main__':
-
-    plotter = plotterABC(logs_dir   ="GNN_training_sampling_2/",
-                         output_dir ="outputplotter",
-                         runs       =[0,1,2,3],
-                         formate    = "png",
-                         dataType   = ["training","validation"],
-                         metrics    = ['auc','loss','precision','accuracy_3','precision_3','recall_3','accuracy_5','precision_5','recall_5','accuracy_7','precision_7','recall_7','f1_7'])
-
+if __name__ == "__main__":
+    plotter = plotterABC(
+        logs_dir="GNN_training_sampling/",
+        output_dir="outputplotter",
+        runs=[0, 1],  # ,2,3],
+        formates=["png"],
+        dataTypes=["training", "validation"],
+        metrics=[
+            "auc",
+            "loss",
+            "precision",
+            "accuracy_3",
+            "precision_3",
+            "recall_3",
+            "accuracy_5",
+            "precision_5",
+            "recall_5",
+            "accuracy_7",
+            "precision_7",
+            "recall_7",
+            "f1_7",
+        ],
+        logY={"auc": True, "loss": True},
+        Axis_limits={"auc": [1e-6, 1], "loss": [0.2, 1]},
+    )
 
     plotter.makePlots()
