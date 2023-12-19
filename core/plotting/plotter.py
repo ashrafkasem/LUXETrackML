7349dd75cdc568cc5a66b981baa6789d007f4237
ashrafkasem <ashrafkasem.asu@gmail.com>
2023-12-19 14:08:48 +0100

    preparing the plotter

---
+++ core/plotting/plotter.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class plotterABC:
+    def __init__(self, metric, logs_dir, output_dir):
+        self.metric = metric
+        self.logs_dir = logs_dir
+        self.output_dir = output_dir
+    
+    def plotLoss():
+        pass 
+
+    def plotROC():
+        pass 
+
+    def plotAcc():
+        pass
+
+    def plotPrec():
+        pass
+
+    def plotRecall():
+        pass
+
+    def plotF1Score():
+        pass
